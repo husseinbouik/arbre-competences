04fcce90820254ba8de5662aa3aa9032e3fb5a9f
husseinbouik <husseinbouik5@gmail.com>
2023-10-05 16:01:56 +0100

    lab-3tiers

--- php-3tiers/add.php
+++ php-3tiers/add.php
@@ -5,4 +5,44 @@ if(isset($_POST['studentSubmitButton']) && $_POST['studentSubmitButton'] == 'Add
     $studentBllObj = new StudentBLO();
     $studentName = $_POST['studentName'];
     $studentRoll = $_POST['studentRoll'];
-$studentDateOfBirth = $_POST['']}
+    $studentEmail = $_POST['steudentEmail'];
+$studentDateOfBirth = $_POST['studentDateOfBirth'];
+$newStudent =new Student(0,$studentName,$studentEmail,$studentDateOfBirth);
+$addStudentResult = $studentBllObj->AddStudent($newStudent);
+if($addStudentResult > 0){
+    header("Location: edit.php?id=". $addStudentResult . '&action=add');
+
+}else {
+    if ($studentBllObj->errorMessage != '') {
+        $errorMessage = $studentBllObj->errorMessage;
+    } else {
+        $errorMessage = 'Record can\'t be added. Operation failed.';
+    }
+}
+}
+$pageTitle = 'Add New Student';
+include_once("Template/header.php");
+?>
+<?php if($errorMessage != ''):?>
+    <div class="alert alert-danger"><?php echo $errorMessage;?></div>
+<?php endif;?>
+<form action="add.php" method="POST" name="studentInfoForm" id="studentInfoForm" class="form-horizontal">
+    <div class="form-group">
+        <label for="studentName" class="col-sm-2 control-label">Name</label>
+        <div class=col-sm-4>
+            <input type="text" name="studentName" id="studentName" class="form-control" placeholder="Name"/>
+</div>
+</div>
+<div class="form-group">
+    <label for="studentEmail" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-4">
+        <input type="email" name="studentEmail" id="studentEmail" class="form-control" placeholder="Email"/>
+</div>
+</div>
+<div class="col-sm-3">
+    <input type="text" name="studentDateOfBirth" id="studentDateOfBirth" class="form-control" placeholder="Date Of Birth"/>
+</div>
+</div>
+</form>
+<?php include_once("Templates/footer.php");?>
+
